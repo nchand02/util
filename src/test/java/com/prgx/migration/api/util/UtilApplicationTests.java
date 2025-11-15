2d1e86ade356507b8b3170dcca841f26901ebecf
Navdeep Singh Chander <navdeepsingh.chander@prgx.com>
2025-11-16 03:11:32 +0530

    Initial commit

---
+++ src/test/java/com/prgx/migration/api/util/UtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgx.migration.api.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
